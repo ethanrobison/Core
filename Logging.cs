714f85b6a7f40520a4c9b69a84db9211251cb7c2
ethan <ethanrobison@protonmail.com>
2020-04-17 15:59:26 -0500

    Add multi-arg functionality to all methods
    Now we can avoid having to construct strings ahead of time, and (at
    least for certain things) only make strings when the logging needs to
    happen).

--- Logging.cs
+++ Logging.cs
@@ -1,42 +1,84 @@
+using System.Diagnostics;
+using System.Text;
+
 namespace Utils
 {
-    // TODO add options for multiple arguments to all of these things
     public static class Logging
     {
-        public static void Log (string message) {
-#if UNITY_EDITOR
-            UnityEngine.Debug.Log(message);
-#endif
+        private static string Stringify (params object[] objects) {
+            var sb = new StringBuilder();
+            foreach (var item in objects) {
+                sb.Append(item.ToString());
+                sb.Append(" ");
+            }
+
+            var res = sb.ToString();
+            sb.Clear();
+            return res;
         }
 
-        public static void Log (object obj) {
-#if UNITY_EDITOR
-            UnityEngine.Debug.Log(obj.ToString());
-#endif
+        [Conditional("UNITY_EDITOR")]
+        public static void Log (object o) => UnityEngine.Debug.Log(o.ToString());
+        [Conditional("UNITY_EDITOR")]
+        public static void Log (object o1, object o2) => UnityEngine.Debug.Log(Stringify(o1, o2));
+        [Conditional("UNITY_EDITOR")]
+        public static void Log (object o1, object o2, object o3)
+            => UnityEngine.Debug.Log(Stringify(o1, o2, o3));
+        [Conditional("UNITY_EDITOR")]
+        public static void Log (object o1, object o2, object o3, object o4)
+            => UnityEngine.Debug.Log(Stringify(o1, o2, o3, o4));
+        [Conditional("UNITY_EDITOR")]
+        public static void Log (object o1, object o2, object o3, object o4, object o5)
+            => UnityEngine.Debug.Log(Stringify(o1, o2, o3, o4, o5));
+
+
+        [Conditional("UNITY_EDITOR")]
+        public static void Assert (bool condition, object o1) {
+            if (!condition) { Error(Stringify(o1)); }
+        }
+        [Conditional("UNITY_EDITOR")]
+        public static void Assert (bool condition, object o1, object o2) {
+            if (!condition) { Error(Stringify(o1, o2)); }
+        }
+        [Conditional("UNITY_EDITOR")]
+        public static void Assert (bool condition, object o1, object o2, object o3) {
+            if (!condition) { Error(Stringify(o1, o2, o3)); }
+        }
+        [Conditional("UNITY_EDITOR")]
+        public static void Assert (bool condition, object o1, object o2, object o3, object o4) {
+            if (!condition) { Error(Stringify(o1, o2, o3, o4)); }
+        }
+        [Conditional("UNITY_EDITOR")]
+        public static void Assert (bool condition, object o1, object o2, object o3, object o4, object o5) {
+            if (!condition) { Error(Stringify(o1, o2, o3, o4, o5)); }
         }
 
-        public static void Assert (bool condition) {
-#if UNITY_EDITOR
-            UnityEngine.Debug.Assert(condition);
-#endif
-        }
+        [Conditional("UNITY_EDITOR")]
+        public static void Warn (object o) => UnityEngine.Debug.LogWarning(o.ToString());
+        [Conditional("UNITY_EDITOR")]
+        public static void Warn (object o1, object o2) => UnityEngine.Debug.LogWarning(Stringify(o1, o2));
+        [Conditional("UNITY_EDITOR")]
+        public static void Warn (object o1, object o2, object o3)
+            => UnityEngine.Debug.LogWarning(Stringify(o1, o2, o3));
+        [Conditional("UNITY_EDITOR")]
+        public static void Warn (object o1, object o2, object o3, object o4)
+            => UnityEngine.Debug.LogWarning(Stringify(o1, o2, o3, o4));
+        [Conditional("UNITY_EDITOR")]
+        public static void Warn (object o1, object o2, object o3, object o4, object o5)
+            => UnityEngine.Debug.LogWarning(Stringify(o1, o2, o3, o4, o5));
 
-        public static void Assert (bool condition, string message) {
-#if UNITY_EDITOR
-            UnityEngine.Debug.Assert(condition, message);
-#endif
-        }
-
-        public static void Warn (string message) {
-#if UNITY_EDITOR
-            UnityEngine.Debug.LogWarning(message);
-#endif
-        }
-
-        public static void Error (string message) {
-#if UNITY_EDITOR
-            UnityEngine.Debug.LogError(message);
-#endif
-        }
+        [Conditional("UNITY_EDITOR")]
+        public static void Error (object o) => UnityEngine.Debug.LogError(o.ToString());
+        [Conditional("UNITY_EDITOR")]
+        public static void Error (object o1, object o2) => UnityEngine.Debug.LogError(Stringify(o1, o2));
+        [Conditional("UNITY_EDITOR")]
+        public static void Error (object o1, object o2, object o3)
+            => UnityEngine.Debug.LogError(Stringify(o1, o2, o3));
+        [Conditional("UNITY_EDITOR")]
+        public static void Error (object o1, object o2, object o3, object o4)
+            => UnityEngine.Debug.LogError(Stringify(o1, o2, o3, o4));
+        [Conditional("UNITY_EDITOR")]
+        public static void Error (object o1, object o2, object o3, object o4, object o5)
+            => UnityEngine.Debug.LogError(Stringify(o1, o2, o3, o4, o5));
     }
 }
