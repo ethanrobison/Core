36b16e8d8497e79b45cfa0251dfa1defe0143930
ethan <ethancrobison@gmail.com>
2020-12-17 13:13:28 -0800

    Add ICtxManager to core
    I'm trying to compile a grouping of all of the code that I want to reuse
    again and again, so we're adding this as well as the "Game" component.

---
+++ Game.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static GameObject Go { get; private set; }
+    public static SessionContext Ctx { get; private set; }
+
+    private void Awake() {
+        Go = gameObject;
+        
+       Ctx = new SessionContext();
+       Ctx.Initialize();
+    }
+}
---
+++ ICtxManager.cs
@@ -0,0 +1,13 @@
+﻿namespace Core
+{
+    public interface ICtxManager
+    {
+        void Initialize();
+        void Shutdown();
+    }
+
+    public interface IUpdateableManager
+    {
+        void Update(float deltaTime);
+    }
+}
