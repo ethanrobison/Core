6964f8b8960fcf0cd9372ca5653410228c54ebad
ethan <ethancrobison@gmail.com>
2021-01-07 08:01:00 -0800

    Improve diagnostics when failing to find child

--- UIUtils.cs
+++ UIUtils.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -8,26 +7,24 @@ namespace Core
 {
     public static class UIUtils
     {
-        public static Transform GetCanvas () {
+        public static Transform GetCanvas() {
             return GameObject.Find("Canvas").transform; // hope there's only ever one canvas
         }
 
-        public static TextMeshProUGUI GetTextMesh (Transform tr, string name) {
-            return FindUICompOfType<TextMeshProUGUI>(tr, name);
+        public static TextMeshProUGUI GetTextMesh(Transform tr, string name) => FindUICompOfType<TextMeshProUGUI>(tr, name);
+        public static TextMeshProUGUI GetTextMesh(GameObject go, string name) => GetTextMesh(go.transform, name);
+
+        public static Button GetButton(GameObject go, string name) => FindUICompOfType<Button>(go, name);
+        public static Button GetAndSetButton(GameObject go, string name, UnityAction onClick) {
+            var btn = GetButton(go, name);
+            btn.SetListener(onClick);
+            return btn;
         }
 
-        public static TextMeshProUGUI GetTextMesh (GameObject go, string name) {
-            return GetTextMesh(go.transform, name);
-        }
-
-        public static Button GetButton (GameObject go, string name) {
-            return FindUICompOfType<Button>(go, name);
-        }
-
-        public static T FindUICompOfType<T> (Transform parent, string name) where T : MonoBehaviour {
+        public static T FindUICompOfType<T>(Transform parent, string name) where T : MonoBehaviour {
             var child = parent.Find(name);
             if (child == null) {
-                Logging.Error("Missing child: " + name);
+                Logging.Error(parent.name, " missing child: ", name);
                 return null;
             }
 
@@ -36,14 +33,14 @@ namespace Core
             return comp;
         }
 
-        public static T FindUICompOfType<T> (GameObject go, string name) where T : MonoBehaviour {
+        public static T FindUICompOfType<T>(GameObject go, string name) where T : MonoBehaviour {
             return FindUICompOfType<T>(go.transform, name);
         }
     }
 
     public static class ButtonExtensions
     {
-        public static void SetListener (this Button btn, UnityAction onclick) {
+        public static void SetListener(this Button btn, UnityAction onclick) {
             btn.onClick.RemoveAllListeners();
             btn.onClick.AddListener(onclick);
         }
