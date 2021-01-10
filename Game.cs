7e9f9fbf52ed16955ebf5b7e8c61548934ba0f53
ethan <ethancrobison@gmail.com>
2021-01-10 13:23:06 -0800

    Add services proto

--- Game.cs
+++ Game.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
+using Core;
 
 public class Game : MonoBehaviour
 {
     public static GameObject Go { get; private set; }
+    public static Services Serv { get; private set; }
     public static SessionContext Ctx { get; private set; }
 
     private void Awake() {
         Go = gameObject;
+
+        Serv = new Services();
+        Serv.Initialize();
 
         Ctx = new SessionContext();
         Ctx.Initialize();
---
+++ Services.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Core
+{
+
+    public class Services
+    {
+        public UiService Ui { get; private set; }
+
+        private bool _initialized;
+
+        public Services() {
+            Ui = new UiService();
+        }
+
+        public void Initialize() {
+            if (_initialized) {
+                Logging.Error("Double initializing services.");
+                return;
+            }
+
+            Ui.Initialize();
+
+            // TODO TEMP
+            Ui.MakeUiElement(UiPrefabs.UiPrefabs.MainMenu);
+
+            _initialized = true;
+        }
+
+        // TODO this happens when the player, e.g., returns to the main menu
+        public void EndSession() {
+            throw new NotImplementedException();
+        }
+
+        // TODO we try to do this when the player is quitting the game to desktop
+        public void ShutDown() {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IServiceManager
+    {
+        void Initialize();
+        void Shutdown();
+    }
+}
