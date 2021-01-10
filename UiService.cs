4994076cbc67ac684a9d5384485c688ea275ba98
ethan <ethancrobison@gmail.com>
2021-01-10 13:26:28 -0800

    Move prefab factory to separate file

--- UiService.cs
+++ UiService.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Core
 {
@@ -10,7 +9,7 @@ namespace Core
 
         public UiService() {
             _canvas = UiUtils.GetCanvas();
-            _factory = new PrefabFactory();
+            _factory = new UiPrefabFactory();
         }
 
         public void Initialize() {
@@ -22,34 +21,6 @@ namespace Core
         }
 
         public GameObject MakeUiElement(UiPrefabs.UiPrefabs prefab) => _factory.MakeUiElement(prefab, _canvas);
-    }
-
-    public abstract class UiPrefabFactory
-    {
-        public GameObject MakeUiElement(UiPrefabs.UiPrefabs prefab, Canvas canvas) => MakeUiElement(prefab, canvas.transform);
-        public abstract GameObject MakeUiElement(UiPrefabs.UiPrefabs prefab, Transform holder = null);
-
-        private static readonly StringBuilder _sb = new StringBuilder();
-        protected string PrefabPathFromEnum(UiPrefabs.UiPrefabs prefabName) {
-            var name = prefabName.ToString();
-            var length = name.Length;
-
-            _sb.Append("Ui/");
-
-            _sb.Append(name[0]);
-            for (int i = 1; i < length; i++) {
-                char c = name[i];
-                if (char.IsUpper(c)) {
-                    _sb.Append(" ");
-                }
-
-                // TODO possibly include parsing rules for inserting /'s
-
-                _sb.Append(c);
-            }
-
-            return _sb.ToStringAndClear();
-        }
     }
 }
 
