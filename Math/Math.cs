d67040d6bf6482ebbb9fd54527b298d1a5cdbdf6
ethan <ethan@ethanrobison.com>
2021-07-30 23:40:41 -0700

    Add Math basics

---
+++ Math/Math.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Math
+{
+    public static class Math
+    {
+        public static int Min(int a, int b) => a <= b ? a : b;
+        public static int Max(int a, int b) => a >= b ? a : b;
+    }
+}
